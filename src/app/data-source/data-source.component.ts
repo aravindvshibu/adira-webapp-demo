32177e35667d3e71fad9c5d893867989cc6d2076
aravindvshibu <aravindvshibu@gmail.com>
2021-12-14 17:56:48 +0530

    3 files modified

--- src/app/data-source/data-source.component.ts
+++ src/app/data-source/data-source.component.ts
@@ -6,7 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./data-source.component.css'],
 })
 export class DataSourceComponent implements OnInit {
+  azure = 'square';
+  s3 = 'square';
+  local = 'square';
+  sftp = 'square';
   constructor() {}
 
+  resetAll() {
+    this.azure = 'square';
+    this.s3 = 'square';
+    this.local = 'square';
+    this.sftp = 'square';
+  }
+  azureActive() {
+    this.resetAll();
+    this.azure = 'squareselected';
+  }
   ngOnInit() {}
 }
